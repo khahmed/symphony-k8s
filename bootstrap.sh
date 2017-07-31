850c514cad8ef28a3828ac2cd7fff4c9c4552a1e
Khalid Ahmed <khalida@Khalids-MacBook-Pro.local>
2017-07-31 10:15:01 -0400

    Use profile.platform instead of profile.ego

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,17 @@ SYM_TOP=/opt/ibm/spectrumcomputing
 CLUSTERADMIN=egoadmin
 HOSTNAME=`hostname`
 
-/bin/su -c "source /profile.ego; egoconfig join $MASTERNAME -f" egoadmin
+/bin/su -c "source $SYM_TOP/profile.platform; egoconfig join $MASTERNAME -f" egoadmin
 if [ "$MASTERNAME" == `hostname` ]; then
     cat /var/tmp/cfc/key1.dat >> /tmp/license.dat
     sed -i -e '$a\' /tmp/license.dat
     cat /var/tmp/cfc/key2.dat >> /tmp/license.dat
-    /bin/su -c "source /profile.ego; egoconfig mghost /shared -f; egoconfig setentitlement /tmp/license.dat" egoadmin
+    /bin/su -c "source $SYM_TOP/profile.platform; egoconfig mghost /shared -f; egoconfig setentitlement /tmp/license.dat" egoadmin
 fi
 
 # Our scripts use uname -a to figure out the platform they are running on. Docker doesn't virtualize that so it reports the 
 # host OS. If the host OS is Ubuntu and Sym container is CentOS scripts get confused
-source /profile.ego
+source $SYM_TOP/profile.platform
 
 chown  $CLUSTERADMIN $SYM_TOP/kernel/work 
 chgrp  $CLUSTERADMIN $SYM_TOP/kernel/work 
